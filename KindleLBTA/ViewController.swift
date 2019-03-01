9fd9eb288f33d824ed1df25ea54d24bb8db3fbae
SJ <vince15dk@gmail.com>
2019-03-01 13:57:16 +0900

    Added pages related to each book

--- KindleLBTA/ViewController.swift
+++ KindleLBTA/ViewController.swift
@@ -38,18 +38,40 @@ class ViewController: UIViewController {
         
         let page1 = Page(number: 1, text: "Text for the first page")
         let page2 = Page(number: 2, text: "This is text for second page")
-//        
-//        print(page1.text)
-//        print(page2.text)
-        
+
         let pages = [page1, page2]
         
-        let book = Book(title: "Steve jobs", author: "Walter Lsaacson", pages: pages  )
-//        print(book.title, book.author)
-//        print("Title: \(book.title), aUTHOR: \(book.author)")
+        let book1 = Book(title: "Steve jobs", author: "Walter Lsaacson", pages: pages  )
+
+//        let firstPage = book.pages[0]
 //
-        let firstPage = book.pages[0]
-        print(firstPage.text)
+//        if page2.text ==  "This is text for second page" {
+//            print(page2.text)
+//        }
+        
+//        book.pages[0]
+//        book.pages[1]
+        for page in book1.pages {
+            if page.number == 1 {
+                print(page.text)
+            }
+        }
+        
+        let book2 = Book(title: "Bill Gates: A Biography", author: "Michael Becraft", pages: [
+            Page(number: 1, text: "first page"),Page(number: 2, text: "second page"),
+            Page(number: 3, text: "third page"),
+            Page(number: 4, text: "fourth page")
+            ])
+//        let bookCollection : [Book]
+//        bookCollection = [book1, book2]
+        
+        for book in [book1, book2] {
+            print("Title : \(book.title) Author: \(book.author)");
+            for page in book.pages {
+                print("Number: \(page.number) Text: \(page.text)")
+            }
+        }
+        
         
     }
 
