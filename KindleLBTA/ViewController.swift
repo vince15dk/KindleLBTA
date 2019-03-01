3df1c2556552e2649c509c291ad83af9f452be23
SJ <vince15dk@gmail.com>
2019-03-01 13:16:00 +0900

    git init

--- KindleLBTA/ViewController.swift
+++ KindleLBTA/ViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
+class Book {
+    let title: String
+    let author: String
+    
+    init(title:String, author:String) {
+        self.title = title
+        self.author = author
+    }
+}
+
+class Page {
+    let number : Int
+    let text : String
+    
+    init(number: Int, text:String){
+        self.number = number
+        self.text = text
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .red
+        
+        let page1 = Page(number: 1, text: "Text for the first page")
+        let page2 = Page(number: 1, text: "This is text for second page")
+        
+        let book = Book(title: "Steve jobs", author: "Walter Lsaacson")
+        print(book.title, book.author)
+        print("Title: \(book.title), aUTHOR: \(book.author)")
+        
     }
 
 
