11ff193190815b455af73023c980300d6f3ec2e9
SJ <vince15dk@gmail.com>
2019-03-01 13:03:33 +0900

    Initial Commit

---
+++ KindleLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KindleLBTA
+//
+//  Created by Suk Joo Kim on 01/03/2019.
+//  Copyright © 2019 Suk Joo Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
